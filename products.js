71f1063336f7073b85f470f428b0c54182f01ad0
erick-serednicki-pernambuco <pernambuco.erick@gmail.com>
2022-11-28 20:47:02 -0300

    concluida task #183905883

--- products.js
+++ products.js
@@ -1,8 +1,15 @@
 const produtos = [
-    {type:"notebook", categoria:"computador", valor:4500.00},
-    {type:"memoria ram", categoria:"memoria", valor:475.00},
-    {type:"HD sata", categoria:"armazenamento", valor:350.00},
-    {type:"mouse", categoria:"periferico", valor:60.00},
-    {type:"teclado", categoria:"periferico", valor:100.00},
-    {type:"SSD ", categoria:"armazenamento", valor:970.00},
-    {type:"cpu", categoria:"processador", valor:45000.00}]
+    {nome:"notebook", categoria:"computador", valor:4500.00},
+    {nome:"memoria ram", categoria:"memoria", valor:475.00},
+    {nome:"HD sata", categoria:"armazenamento", valor:350.00},
+    {nome:"mouse", categoria:"periferico", valor:60.00},
+    {nome:"teclado", categoria:"periferico", valor:100.00},
+    {nome:"SSD ", categoria:"armazenamento", valor:970.00},
+    {nome:"cpu", categoria:"processador", valor:45000.00}]
+    
+    function listar_por_categoria (categoria) {
+        let lista = produtos.filter(produtos=> produtos.categoria == categoria);
+        return console.log(lista);
+    }
+
+listar_por_categoria("periferico"); // inserir string da categoria
