7c0fafa6b7df67ad9f9ab595f35669b1f1d56d05
erick-serednicki-pernambuco <pernambuco.erick@gmail.com>
2022-11-28 20:22:21 -0300

    incio dor projeto

---
+++ products.js
@@ -0,0 +1,8 @@
+const produtos = [
+    {type:"notebook", categoria:"computador", valor:4500.00},
+    {type:"memoria ram", categoria:"memoria", valor:475.00},
+    {type:"HD sata", categoria:"armazenamento", valor:350.00},
+    {type:"mouse", categoria:"periferico", valor:60.00},
+    {type:"teclado", categoria:"periferico", valor:100.00},
+    {type:"SSD ", categoria:"armazenamento", valor:970.00},
+    {type:"cpu", categoria:"processador", valor:45000.00}]
