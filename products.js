217fc694bdcc70875db0f9d0ad47e19b8cc35eb6
erick-serednicki-pernambuco <pernambuco.erick@gmail.com>
2022-11-28 21:19:34 -0300

    finalizada task #183906151

--- products.js
+++ products.js
@@ -10,6 +10,26 @@ const produtos = [
     function listar_por_categoria (categoria) {
         let lista = produtos.filter(produtos=> produtos.categoria == categoria);
         return console.log(lista);
-    }
+    };
+
+    function listar_por_valor(valor_minimo, valor_maximo){
+        if(!isNaN(valor_maximo) === false){
+            return console.log("Apenas valores numéricos");
+        }
+        if(!isNaN(valor_minimo) === false){
+            return console.log("Apenas valores numéricos");
+        };
+        if(valor_minimo > valor_maximo){
+            let aux;
+            aux = valor_minimo;
+            valor_minimo = valor_maximo;
+            valor_maximo = aux;
+        }
+        let lista = produtos.filter(produtos => 
+            Number(produtos.valor) >= Number(valor_minimo) && Number(produtos.valor) <= Number(valor_maximo));
+        return console.log(lista);
+    };
 
 listar_por_categoria("periferico"); // inserir string da categoria
+
+listar_por_valor(50.00 ,100.00); // inserir valor do produto
